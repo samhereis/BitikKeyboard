c237ab211dd2e350a1c465e18d1569409892e2d4
samhereis <samagan888@gmail.com>
2025-12-20 21:21:52 +0600

    new translations

--- BitikKeyboard/app/build.gradle.kts
+++ BitikKeyboard/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.shoktuk.shoktukkeyboard"
         minSdk = 25
         targetSdk = 36
-        versionCode = 36
+        versionCode = 37
         versionName = "3.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- BitikKeyboard/app/src/main/java/com/shoktuk/shoktukkeyboard/project/systems/localization/LocalizationManager.kt
+++ BitikKeyboard/app/src/main/java/com/shoktuk/shoktukkeyboard/project/systems/localization/LocalizationManager.kt
@@ -8,9 +8,9 @@ import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 enum class Language(val code: String, val displayName: String) {
     KY_L("ky_latin", "🇰🇬 Qırğız"),
     KY_K("ky_kiril", "🇰🇬 Кыргыз"),
-    //TR("tr", "🇹🇷 Türkçe"),
-    //KZ("kz", "🇰🇿 Қазақша"),
-    //AZ("az", "🇦🇿 Azərbaycan"),
+    TR("tr", "🇹🇷 Türkçe"),
+    KZ("kz", "🇰🇿 Қазақша"),
+    AZ("az", "🇦🇿 Azərbaycan"),
     EN("en", "🇺🇸 English"),
     RU("ru", "🇷🇺 Русский")
 }
