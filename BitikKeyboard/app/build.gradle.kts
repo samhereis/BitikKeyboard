1e1e789793d00ff4ee40d9cb262e51ccef10a129
samhereis <samagan888@gmail.com>
2025-12-23 09:57:16 +0600

    save

--- BitikKeyboard/app/build.gradle.kts
+++ BitikKeyboard/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.shoktuk.shoktukkeyboard"
         minSdk = 25
         targetSdk = 36
-        versionCode = 38
+        versionCode = 39
         versionName = "3.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
